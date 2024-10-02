e0c22273b44e9262f11148d7c8a6ef1b06fa0e52
Andrei Maria <f.maria@student.utwente.nl>
2024-10-02 20:10:14 +0200

    Added logo, configured files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
+          src="/logo.png" // Ensure the image source is correct
+          alt="Business logo"
           width={180}
-          height={38}
+          height={50}
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
@@ -22,7 +22,6 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
