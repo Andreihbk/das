263708e313b4ded7ebba6d98b27fbde231a94b54
Dani1232312 <105224326+Dani1232312@users.noreply.github.com>
2024-10-03 18:34:17 +0200

    added testcases

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function About() {
     return (
       <div className="flex flex-col items-center justify-center min-h-screen p-8">
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function Contact() {
     return (
       <div className="flex flex-col items-center justify-center min-h-screen p-8">
