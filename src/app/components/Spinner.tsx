fd366c4f775f8dc7e2c1794caecb11bac86358a8
Dani1232312 <105224326+Dani1232312@users.noreply.github.com>
2024-10-04 21:02:53 +0200

    fix

--- src/app/components/Spinner.tsx
+++ src/app/components/Spinner.tsx
@@ -1,4 +1,6 @@
 // components/Spinner.js
+import React from 'react';
+
 export default function Spinner() {
     return (
       <div className="flex items-center justify-center h-screen bg-white bg-opacity-80 dark:bg-gray-900 dark:bg-opacity-80 fixed inset-0 z-50">
