c4d6c3b8e6b1b722003a2ca4b01e9b440cd8dc6e
Dani1232312 <105224326+Dani1232312@users.noreply.github.com>
2024-10-04 11:51:13 +0200

    added navbar to pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import Navbar from "../../components/Navbar"; // Import the Navbar
 
 export default function About() {
     return (
+      <div className="min-h-screen">
+      <Navbar />
       <div className="flex flex-col items-center justify-center min-h-screen p-8">
         <h1 className="text-3xl font-bold mb-4">About Us</h1>
         <p className="text-lg text-center">
           Welcome to our showcase website. We are dedicated to providing quality services and products to our customers.
         </p>
       </div>
+      </div>
+
     );
   }
   
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import Navbar from "../../components/Navbar"; // Import the Navbar
 
 export default function Contact() {
     return (
+      <div className="min-h-screen">
+      <Navbar /> 
       <div className="flex flex-col items-center justify-center min-h-screen p-8">
         <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
         <p className="text-lg text-center mb-4">
           Feel free to reach out to us via email at: <strong>contact@example.com</strong>
         </p>
+      </div>
       </div>
     );
   }
