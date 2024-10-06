39f4b60663bdb4e6e9d9ddc8fcdffa0d1cf004ba
Dani1232312 <105224326+Dani1232312@users.noreply.github.com>
2024-10-06 14:00:25 +0200

    update tests

--- src/app/about/page.test.tsx
+++ src/app/about/page.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import About from './page'; // Adjust the path based on your directory structure
+import About from './page';
 
 describe('About Page', () => {
   it('renders the About page with the correct content', () => {
@@ -16,11 +16,19 @@ describe('About Page', () => {
     expect(screen.getByText(/dedicated to providing quality services and products/i)).toBeInTheDocument();
   });
 
-  it('renders the Navbar component', () => {
+  it('renders the correct sections', () => {
     render(<About />);
 
-    // Check if the Navbar is rendered
-    const navbar = screen.getByRole('navigation');
-    expect(navbar).toBeInTheDocument();
+    // Check for the "Our Mission" section
+    const ourMissionElements = screen.getAllByText(/our mission/i);
+    expect(ourMissionElements.length).toBeGreaterThan(0); // Check at least one instance
+
+    // Check for the "Meet the Team" section
+    const meetTheTeamElements = screen.getAllByText(/meet the team/i);
+    expect(meetTheTeamElements.length).toBeGreaterThan(0); // Check at least one instance
+
+    // Check for "Our History" section
+    const ourHistoryElements = screen.getAllByText(/our history/i);
+    expect(ourHistoryElements.length).toBeGreaterThan(0); // Check at least one instance
   });
 });
