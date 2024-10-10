e176f2f0304e50f4cb617893d87a13e699174ae4
Daniel Botnarenco <105224326+Dani1232312@users.noreply.github.com>
2024-10-10 20:09:10 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    swcMinify: true, //  Use the SWC compiler for faster builds and smaller bundles
+    swcMinify: true, // Use the SWC compiler for faster builds and smaller bundles
 };
 
 export default nextConfig;
